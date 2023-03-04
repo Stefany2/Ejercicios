84155caed9b65d80b50e42ec2e0efd23e11f5a52
Stefany2 <1459565@senati.pe>
2023-03-04 09:43:06 -0500

    Ejercicio

---
+++ Ejercicio.py
@@ -0,0 +1,9 @@
+#Escribir un codigo de programacion donde me permite imprimir el primer
+# y el ultimo valor de la lista, for ejemplo:
+#list= ["red","blue",...,"white"]
+
+list = ["red","blue","....","white"]
+print(list)
+print(list[1])
+list[2]= "naranjado"
+print(list)
